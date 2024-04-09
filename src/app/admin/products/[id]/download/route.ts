e0ae403eef7acf960a5301cf601900204cee4bfb
Anirudh Memani <nemanianirudh@gmail.com>
2024-04-09 20:03:52 +0530

    admin page complete

---
+++ src/app/admin/products/[id]/download/route.ts
@@ -0,0 +1,29 @@
+import prisma from "@/db/db";
+import { notFound } from "next/navigation";
+import { NextRequest, NextResponse } from "next/server";
+import fs from "fs/promises";
+
+export async function GET(
+    req: NextRequest,
+    { params: { id } }: { params: { id: string } }
+) {
+    const product = await prisma.product.findUnique({
+        where: { id },
+        select: { filePath: true, name: true },
+    });
+
+    if (!product) {
+        return notFound();
+    }
+
+    const { size } = await fs.stat(product.filePath);
+    const file = await fs.readFile(product.filePath);
+    const extension = product.filePath.split(".").pop();
+
+    return new NextResponse(file, {
+        headers: {
+            "Content-Disposition": `attachment; filename=${product.name}.${extension}`,
+            "Content-Length": size.toString(),
+        },
+    });
+}
---
+++ src/middleware.ts
@@ -0,0 +1,45 @@
+import { NextRequest, NextResponse } from "next/server";
+import { isValidPassword } from "./lib/isValidPassword";
+
+export async function middleware(req: NextRequest) {
+    const authenticated = await isAuthentication(req);
+
+    if (!authenticated) {
+        return new NextResponse("Unauthorized", {
+            status: 401,
+            headers: {
+                "WWW-Authenticate": "Basic",
+            },
+        });
+    }
+}
+
+async function isAuthentication(req: NextRequest): Promise<boolean> {
+    const authHeader =
+        req.headers.get("authorization") || req.headers.get("Authorization");
+
+    if (!authHeader) {
+        return false;
+    }
+
+    /**
+     * The authHeader will look something like "Base jsakhdjkasd"
+     * We take the second base64 encoded value and decode it using toString()
+     * and the the result would look like username:password, so we split it using ":" and get the username and password as a tuple
+     */
+    const [username, password] = Buffer.from(authHeader.split(" ")[1], "base64")
+        .toString()
+        .split(":");
+
+    return (
+        username === process.env.ADMIN_USERNAME &&
+        (await isValidPassword(
+            password,
+            process.env.HASHED_ADMIN_PASSWORD as string
+        ))
+    );
+}
+
+export const config = {
+    matcher: "/admin/:path*",
+};
